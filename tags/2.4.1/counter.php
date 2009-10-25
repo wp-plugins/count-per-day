998587f69ddd22a8db384974d946eaa144a0a2c7
Tom Braider <Tom Braider@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-25 13:54:10 +0000

    git-svn-id: https://plugins.svn.wordpress.org/count-per-day@166785 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/2.4.1/counter.php
+++ tags/2.4.1/counter.php
@@ -3,7 +3,7 @@
 Plugin Name: Count Per Day
 Plugin URI: http://www.tomsdimension.de/wp-plugins/count-per-day
 Description: Counter, shows reads per page; today, yesterday, last week, last months ... on dashboard and widget.
-Version: 2.4
+Version: 2.4.1
 License: GPL
 Author: Tom Braider
 Author URI: http://www.tomsdimension.de
