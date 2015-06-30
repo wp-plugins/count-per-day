b98ce0cb56c5771abd591bec86f862fbcfda3af0
Tom Braider <Tom Braider@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-30 06:41:58 +0000

    Security fix in backup download
    git-svn-id: https://plugins.svn.wordpress.org/count-per-day/trunk@1190096 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- download.php
+++ download.php
@@ -5,15 +5,15 @@
  */
 if ( empty($_GET['f']) || empty($_GET['n']) )
 	die('no way');
-$file = sys_get_temp_dir().'/'.$_GET['f'];
-if (strpos($file, '..') !== false
-	&& strpos($file, 'cpdexport') !== 0
-	&& strpos($file, 'cpdbackup') !== 0
-	)
+$file = sys_get_temp_dir().'/'.strip_tags($_GET['f']);
+if ( strpos($file, '..') !== false )
+	die('no way');
+if ( strpos(basename($file), 'cpdexport') !== 0
+	&& strpos(basename($file), 'cpdbackup') !== 0 )
 	die('no way');
 if (!file_exists($file))
 	die('file not found');
-$name = stripslashes($_GET['n']);
+$name = stripslashes(strip_tags($_GET['n']));
 if (substr($name, -2) == 'gz')
 	header('Content-Type: application/x-gzip');
 else if (substr($name, -3) == 'csv')
